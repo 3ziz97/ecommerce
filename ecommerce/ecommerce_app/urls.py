3b3bd7b8286aef04966d9a4b0ac4b36a7cb8e927
3ziz97 <123443177+3ziz97@users.noreply.github.com>
2023-02-21 12:14:33 +0200

    Delete ecommerce directory

--- ecommerce/ecommerce_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-app_name = "ecommerce_app"
-
-urlpatterns =[
-    path("", HomeView.as_view(), name="home"),
-    path("about/", AboutView.as_view(), name="about"),
-    path("contactus/", ContactView.as_view(), name="contactus"),
-]
